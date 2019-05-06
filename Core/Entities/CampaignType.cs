68013a774054dddd09523aba57a1e022a9cab7d9
hxq1988 <hxq1988@DESKTOP-605S6L4>
2019-05-06 17:33:23 +0700

    update db

---
+++ Core/Entities/CampaignType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class CampaignType : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string Data { get; set; }
+    }
+}
