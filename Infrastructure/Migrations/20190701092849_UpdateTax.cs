a81aa3d551b3dec934cd40c573854cc3a415159d
hxq1988 <hxq1988@DESKTOP-EPUAOBU>
2019-07-01 17:23:15 +0700

    update tax db

---
+++ Infrastructure/Migrations/20190701092849_UpdateTax.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class UpdateTax : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TaxIdNumber",
+                table: "Agency",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TaxIdNumber",
+                table: "Agency");
+        }
+    }
+}
