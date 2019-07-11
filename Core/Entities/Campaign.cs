12f8866e65b6ce55657b2bf0d14a30a2043d048f
hxq1988 <hxq1988@DESKTOP-EPUAOBU>
2019-07-11 17:27:31 +0700

    update status

--- Core/Entities/Campaign.cs
+++ Core/Entities/Campaign.cs
@@ -13,11 +13,11 @@ namespace Core.Entities
         public Agency Agency { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        
+
         //Mô tả, Nội dung hoặc được link của campaign để người dùng thao tác....
         public string Data { get; set; }
         public string Image { get; set; }
-   
+
         public string Requirement { get; set; }
 
         public string SystemNote { get; set; }
@@ -64,20 +64,26 @@ namespace Core.Entities
         Canceled = 5,
         [DisplayName("Lỗi")]
         Error = 6,
+
+        //[DisplayName("Chờ phê duyệt")]
+        //ChoPheDuyet = 0,
+        //[DisplayName("Bắt đầu")]
+        //BatDau = 1,
+        //[DisplayName("Thực hiện")]
+        //ThucHien = 2,
+        //[DisplayName("Theo dõi")]
+        //TheoDoi = 3,
+        //[DisplayName("Hoàn thành")]
+        //HoanThanh = 4,
+        //[DisplayName("Kết thúc")]
+        //KetThuc = 5,
+        //[DisplayName("Đã Hủy")]
+        //DaHuy = 5,
+        //[DisplayName("Lỗi")]
+        //Loi = 6,
     }
 
-    /*
-     [DisplayName("Chờ phê duyệt")]
-        ChoPheDuyet = 1,
-        [DisplayName("Thực hiện chiến dịch")]
-        ThucHien = 2,
-        [DisplayName("Theo dõi chiến dịch")]
-        TheoDoi = 3,
-        [DisplayName("Đã hoàn thành")]
-        Completed = 4,
-        [DisplayName("Đã Hủy")]
-        Canceled = 5,
-        [DisplayName("Lỗi")]
-        Error = 6,
-     */
+
+
+
 }
