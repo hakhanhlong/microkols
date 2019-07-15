fff82fd0f9816b3fd582e684040311b877227cb8
hxq1988 <hxq1988@DESKTOP-EPUAOBU>
2019-07-15 17:34:13 +0700

    update transaction cashout

---
+++ Infrastructure/Migrations/20190715103348_AddTransactionCashout.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddTransactionCashout : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CashoutDate",
+                table: "Transaction",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCashOut",
+                table: "Transaction",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CashoutDate",
+                table: "Transaction");
+
+            migrationBuilder.DropColumn(
+                name: "IsCashOut",
+                table: "Transaction");
+        }
+    }
+}
