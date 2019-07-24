c70b4857f29c708e80a84c83b29cd605f0f2286f
hxq1988 <hxq>
2019-07-24 07:12:00 +0700

    update rename

--- Website/gulpfile.js
+++ Website/gulpfile.js
@@ -3,6 +3,9 @@ var gulp = require("gulp"),
     fs = require("fs"),
     //less = require("gulp-less"),
     sass = require("gulp-sass"),
+    cssnano = require('gulp-cssnano'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
     concat = require('gulp-concat');
 
 
@@ -10,6 +13,9 @@ var gulp = require("gulp"),
 gulp.task('pack-js', function () {
     return gulp.src(['wwwroot/js/app.js', 'wwwroot/js/app.*.js'])
         .pipe(concat('app.js'))
+        .pipe(gulp.dest('wwwroot/build/js'))
+        .pipe(rename('uglify.js'))
+        .pipe(uglify())
         .pipe(gulp.dest('wwwroot/build/js'));
 });
 
