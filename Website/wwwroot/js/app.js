47c0b9a17e872293a8b9a3ff86e35a0cab3ea6a7
hxq1988 <hxq1988@DESKTOP-EPUAOBU>
2019-11-15 09:47:17 +0700

    update fb feed

--- Website/wwwroot/js/app.js
+++ Website/wwwroot/js/app.js
@@ -214,6 +214,25 @@
             max: $.validator.format("Hãy nhập từ {0} trở xuống."),
             min: $.validator.format("Hãy nhập từ {0} trở lên.")
         });
+
+
+
+        $('.nav-fbfeed').click(function (e) {
+            e.preventDefault();
+
+            FB.login(function (response) {
+                if (response.status === 'connected') {
+                    var token = response.authResponse.accessToken;
+
+                    $('#frmUpdatefbpostToken').val(token);
+                    $('#frmUpdatefbpost').submit();
+                } else {
+                    alert('Bạn cần cập nhật quyền trên hệ thống của Facebook');
+                }
+
+            }, { scope: 'user_posts' });
+
+        });
     }
 
 
