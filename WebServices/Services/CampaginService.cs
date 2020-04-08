397adb7135c4c4468cf2948210a51c1a003df8ae
HA KHANH LONG <HA KHANH LONG@DESKTOP-98E0D61>
2020-04-09 01:33:59 +0700

    sync

--- WebServices/Services/CampaginService.cs
+++ WebServices/Services/CampaginService.cs
@@ -1440,60 +1440,60 @@ namespace WebServices.Services
 
                 foreach (var id in campaignids)
                 {
+
                     BackgroundJob.Enqueue<ICampaignService>(m => m.RunCheckingLockedStatus(id));
-
-
                 }
             }
             else
             {
-                var username = "system";
-                var campaign = await _campaignRepository.GetByIdAsync(campaignid);
-                if (campaign != null)
-                {
-                    if(campaign.Status == CampaignStatus.Locked)
+
+
+                try {
+                    var username = "system";
+                    var campaign = await _campaignRepository.GetByIdAsync(campaignid);
+                    if (campaign != null)
                     {
-                        /*Kiểm tra thời gian thực hiện chiến dịch kết thúc có bé hơn ngày giờ hiện tại hay không để hủy bỏ chiến dịch*/
-                        if (campaign.ExecutionEnd.HasValue)
+                        if (campaign.Status == CampaignStatus.Locked)
                         {
-                            if(campaign.ExecutionEnd.Value <= DateTime.Now)
+                            /*Kiểm tra thời gian thực hiện chiến dịch kết thúc có bé hơn ngày giờ hiện tại hay không để hủy bỏ chiến dịch*/
+                            if (campaign.ExecutionEnd.HasValue)
                             {
-
-                                campaign.Status = CampaignStatus.Canceled;
-                                campaign.UserModified = username;
-                                campaign.DateModified = DateTime.Now;
-                                await _campaignRepository.UpdateAsync(campaign);
-
-                                /*Kiểm tra xem đã thanh toán chiến dịch lần nào chưa, nếu rồi gửi thông báo tạo lệnh rút tiền*/
-                                string _message = string.Format("Chiến dịch {0} đã bị hủy, vì thời gian thực hiện đã hết và chưa thanh toán.", campaign.Title);
-                                var payment = await _campaignRepository.GetCampaignPaymentByAgency(campaign.AgencyId, campaign.Id);
-                                if (payment != null)
+                                if (campaign.ExecutionEnd.Value <= DateTime.Now)
                                 {
-                                    if(payment.TotalPaidAmount > 0)
+
+                                    campaign.Status = CampaignStatus.Canceled;
+                                    campaign.UserModified = username;
+                                    campaign.DateModified = DateTime.Now;
+                                    await _campaignRepository.UpdateAsync(campaign);
+
+                                    /*Kiểm tra xem đã thanh toán chiến dịch lần nào chưa, nếu rồi gửi thông báo tạo lệnh rút tiền*/
+                                    string _message = string.Format("Chiến dịch {0} đã bị hủy, vì thời gian thực hiện đã hết và chưa thanh toán.", campaign.Title);
+                                    var payment = await _campaignRepository.GetCampaignPaymentByAgency(campaign.AgencyId, campaign.Id);
+                                    if (payment != null)
                                     {
-                                        _message += string.Format("Hãy tạo lệnh rút {0} từ chiến dịch", payment.TotalPaidAmount.ToPriceText());                                        
+                                        if (payment.TotalPaidAmount > 0)
+                                        {
+                                            _message += string.Format("Hãy tạo lệnh rút {0} từ chiến dịch", payment.TotalPaidAmount.ToPriceText());
+                                        }
                                     }
+                                    await _notificationRepository.CreateNotification(NotificationType.CampaignCanceled,
+                                    EntityType.Agency, campaign.AgencyId, campaign.Id, _message);
+                                    try
+                                    {
+                                        string _msg = string.Format("Chiến dịch \"{0}\" của doanh nghiệp \"{1}\", đã bị hủy. vì thời gian thực hiện đã hết và chưa thanh toán", campaign.Title, campaign.UserCreated);
+                                        string _data = "Campaign";
+                                        await _notificationRepository.CreateNotification(campaign.Id, EntityType.System, 0, NotificationType.CampaignCanceled, _msg, _data);
+                                    }
+                                    catch// tranh loi lam crash 
+                                    { }
                                 }
-                                await _notificationRepository.CreateNotification(NotificationType.CampaignCanceled,
-                                EntityType.Agency, campaign.AgencyId, campaign.Id, _message);
-                                try
-                                {
-                                    string _msg = string.Format("Chiến dịch \"{0}\" của doanh nghiệp \"{1}\", đã bị hủy. vì thời gian thực hiện đã hết và chưa thanh toán", campaign.Title, campaign.UserCreated);
-                                    string _data = "Campaign";
-                                    await _notificationRepository.CreateNotification(campaign.Id, EntityType.System, 0, NotificationType.CampaignCanceled, _msg, _data);
-                                }
-                                catch// tranh loi lam crash 
-                                { }
                             }
                         }
-
-                        
-
-
-
                     }
                 }
-                return;
+                catch { }
+                
+                //return;
             }
         }
         //######################################################################################################################################
