6d83e1d67586a6f5bbc02c5cab309a8468f4074d
HA KHANH LONG <HA KHANH LONG@DESKTOP-98E0D61>
2020-05-31 13:29:55 +0700

    sync

--- BackOffice/Controllers/LandingPageController.cs
+++ BackOffice/Controllers/LandingPageController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using WebServices.ViewModels;
 
@@ -11,9 +12,11 @@ namespace BackOffice.Controllers
     public class LandingPageController : Controller
     {
 
-        public LandingPageController()
-        {
+        private readonly IQnARepository _IQnARepository;
 
+        public LandingPageController(IQnARepository __IQnARepository)
+        {
+            _IQnARepository = __IQnARepository;
         }
 
         public IActionResult Index()
@@ -32,9 +35,33 @@ namespace BackOffice.Controllers
             return View("UpdateQnA", new QnAViewModel() { Id = 0 });
         }
 
-        public IActionResult UpdateQnA()
+        [HttpGet]
+        public async Task<IActionResult> UpdateQnA(int Id = 0)
         {
-            return View();
+            if (Id != 0)
+            {
+                var qna = await _IQnARepository.GetByIdAsync(Id);
+                if (qna != null)
+                {
+                    return View(new QnAViewModel()
+                    {
+                        Id = qna.Id,
+                        Question = qna.Question,
+                        Answer = qna.Answer,
+                        IsActive = qna.IsActive,
+                        Order = qna.Order
+                    });
+
+                }
+                else
+                {
+                    TempData["MessageError"] = string.Format("{0}", "QnA empty, do not exist!");
+                }
+
+            }
+
+            return View(new QnAViewModel() { Id = 0 });
+
         }
 
 
